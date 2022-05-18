db0730531c45874437ac1fbb2f56235a0bb3767a
MOHAMMED <mirasifali7777@gmail.com>
2022-05-18 15:33:41 -0500

    upload project

---
+++ src/main/java/devopsproject/Home.java
@@ -0,0 +1,9 @@
+package devopsproject;
+
+public class Home {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the world of Java Training from Code Lab Technology");
+	}
+
+}
