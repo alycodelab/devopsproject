6c685840a811344d9f8a5ead00a360361b2623bf
MOHAMMED <mirasifali7777@gmail.com>
2022-05-18 15:38:00 -0500

    second commit

--- src/main/java/devopsproject/Home.java
+++ src/main/java/devopsproject/Home.java
@@ -4,6 +4,8 @@ public class Home {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the world of Java Training from Code Lab Technology");
+		
+		System.out.println("Hello Sailaja, Kavitha and Sridevi");
 	}
 
 }
